2087bc164ee816ce1ea7782514c062eb746fd733
hectorvegaloza <-vega314212@gmail.com>
2024-05-22 20:04:03 -0500

    agregando express

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 'use strict';
+const express = require("express")
 
-console.log("hola mundo");
+const servidor = express();
+
+servidor.get("/", (solicitud,respuesta)=>{
+    respuesta.json({
+        saludo: "Hola chamos"
+    })
+});
+
+ servidor.get("/xxx", (solicitud, respuesta)=>{
+    respuesta.redirect(301, "https://www.ucundinamarca.edu.co/index.php/servicios/plataforma-institucional");
+})
+ 
+
+servidor.listen(3000);
+console.log("servidor comando");

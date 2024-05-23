9048d88930bd9da731458f60140e8a95401693d4
hectorvegaloza <-vega314212@gmail.com>
2024-05-22 21:41:58 -0500

    express

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 'use strict';
+
 const express = require("express")
 
 const servidor = express();
 
 servidor.get("/", (solicitud,respuesta)=>{
     respuesta.json({
-        saludo: "Hola chamos"
+        saludo: "bendiciones para todos"
     })
 });
 
@@ -15,4 +16,4 @@ servidor.get("/", (solicitud,respuesta)=>{
  
 
 servidor.listen(3000);
-console.log("servidor comando");
+console.log("servidor corriendo en el puerto 3000");
